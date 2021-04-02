770efd6c749ff878d00e8222fb543f810731283a
KarpoKoodaa <karpokoodaa@gmail.com>
2021-04-02 07:15:59 +0300

    keypad config + test

--- MEGA2560/main.c
+++ MEGA2560/main.c
@@ -16,18 +16,26 @@
 
 
 
+
 int
 main (void)
 {
+    uint8_t key;
+    KEYPAD_Init();
+    
     lcd_init(LCD_DISP_ON);
     lcd_clrscr();
 
     lcd_puts("Alarm System\n");
     lcd_puts("ACTIVATED");
+    _delay_ms(2000);
+    lcd_clrscr();
 
     while (1)
     {
         /* code */
+        key = KEYPAD_GetKey();
+        lcd_putc(key);
     }
     
 }
